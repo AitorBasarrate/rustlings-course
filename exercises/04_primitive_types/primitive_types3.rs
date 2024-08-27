3536ec9687822cf1a247d4d34c5f98a84bb4a5e9
Aitor Basarrate <basarrate.aitor@gmail.com>
2024-08-27 20:29:59 +0200

    04 primitive types done

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,7 +1,11 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
-
+    let b = 1;
+    let a = [b;100];
+    for x in a.iter() {
+        print!("{}", x);
+    }
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
