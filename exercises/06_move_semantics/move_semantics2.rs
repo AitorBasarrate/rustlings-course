b674dac6f2c31709ae51294e0be685bee09a327a
Aitor Basarrate <basarrate.aitor@gmail.com>
2024-08-31 19:42:26 +0200

    semantics 1 and 2 completed

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -2,7 +2,6 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(88);
-
     vec
 }
 
@@ -19,8 +18,9 @@ mod tests {
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
+        let vec0_copy = vec0.clone();
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0_copy);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
